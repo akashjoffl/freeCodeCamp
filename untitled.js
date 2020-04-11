3eee233c54cee5c5826d57eb6dcf624a7e1b77df
Akash Jeganath <akashjoffl@icloud.com>
2020-04-12 02:56:39 +0530

    Object Oriented Programming: Create a Basic JavaScript Object

--- untitled.js
+++ untitled.js
@@ -1,12 +1,4 @@
-function chunkArrayInGroups(arr, size) {
-
-    let result = [];
-    // creating an empty array to store the chunk
-    for (let i = 0; i < arr.length; i += size) {
-        console.log(arr[i]);
-        result.push(arr.slice(i, i + size));
-    }
-    return result;
-}
-
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+let dog = {
+    name: 'Bronie',
+    numLegs: 2
+};
