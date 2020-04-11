3f377632cea931a2035e7b49d17aedbce48ba23d
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 06:13:04 +0530

    Basic Algorithm Scripting: Title Case a Sentence

--- untitled.js
+++ untitled.js
@@ -1,13 +1,26 @@
-function findElement(arr, func) {
-    let num = 0;
-    for (let i = 0; i < arr.length; i++) {
-        num = arr[i];
-        console.log('*', num);
-        if (func(num)) {
-            return num;
+function titleCase(str) {
+    let answerStr = '';
+    let words = str.split(" ");
+    // console.log(words);
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        //    console.log(word)
+
+        for (let j = 0; j < word.length; j++) {
+            //console.log(word[j]);
+            if (j === 0) {
+                answerStr += word[j].toUpperCase();
+            } else {
+                answerStr += word[j].toLowerCase();
+            }
+            // console.log(answerStr);
         }
+        answerStr += ' ';
+        // making space inbetween words *this also adds a space at the last word's end 
     }
-    return undefined;
+    return answerStr.slice(0, -1);
+    // using slice method to remove the space from last element's last charector 
+    //   console.log(word);
 }
 
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+console.log(titleCase("I'm a little tea pot"));
