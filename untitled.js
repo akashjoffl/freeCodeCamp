222f55cc0f9db7763bb325b493fdce2875016f49
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 07:51:30 +0530

    Basic Algorithm Scripting: Falsy Bouncer

--- untitled.js
+++ untitled.js
@@ -1,26 +1,13 @@
-function titleCase(str) {
-    let answerStr = '';
-    let words = str.split(" ");
-    // console.log(words);
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i];
-        //    console.log(word)
-
-        for (let j = 0; j < word.length; j++) {
-            //console.log(word[j]);
-            if (j === 0) {
-                answerStr += word[j].toUpperCase();
-            } else {
-                answerStr += word[j].toLowerCase();
-            }
-            // console.log(answerStr);
-        }
-        answerStr += ' ';
-        // making space inbetween words *this also adds a space at the last word's end 
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        // listing all the inputs
+        console.log(arr[i]);
+        if (arr[i]) newArray.push(arr[i]);
+        //if arr[1] is true push it to newArray or return
     }
-    return answerStr.slice(0, -1);
-    // using slice method to remove the space from last element's last charector 
-    //   console.log(word);
+
+    return newArray;
 }
 
-console.log(titleCase("I'm a little tea pot"));
+bouncer([7, "ate", "", false, 9]);
