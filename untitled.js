7c43a8de36eafdd6d3237a0cea8568ce87dc040f
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 08:25:48 +0530

    Basic Algorithm Scripting: Mutations

--- untitled.js
+++ untitled.js
@@ -1,18 +1,14 @@
-function getIndexToIns(arr, num) {
-    arr.push(num);
-    //console.log(arr);
-    // adding num array element into arr array
-    arr.sort(function (a, b) {
-        return a - b
-    });
-    console.log(arr);
-    // sorting array in order
-
-    let indexArray = arr.indexOf(num)
-    // finding and returning the index
-    return indexArray;
-
-
+function mutation(arr) {
+    let target = arr[0].toLowerCase();
+    let test = arr[1].toLowerCase();
+    // making everything lowerCase so it will be easier to compare tho.
+    for (let i = 0; i < test.length; i++) {
+        // listing all the letters of 2nd array element
+        if (target.indexOf(test[i]) < 0)
+            // if no elements (0) found; returning false; else true; 
+            return false;
+    }
+    return true;
 }
 
-console.log(getIndexToIns([5, 3, 20, 3], 5));
+console.log(mutation(["hello", "hey"]));
