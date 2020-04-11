86e0e8e9a1bd63a792305dd7ba3e3eca34e821d8
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 08:48:32 +0530

    Basic Algorithm Scripting: Chunky Monkey

--- untitled.js
+++ untitled.js
@@ -1,14 +1,12 @@
-function mutation(arr) {
-    let target = arr[0].toLowerCase();
-    let test = arr[1].toLowerCase();
-    // making everything lowerCase so it will be easier to compare tho.
-    for (let i = 0; i < test.length; i++) {
-        // listing all the letters of 2nd array element
-        if (target.indexOf(test[i]) < 0)
-            // if no elements (0) found; returning false; else true; 
-            return false;
+function chunkArrayInGroups(arr, size) {
+
+    let result = [];
+    // creating an empty array to store the chunk
+    for (let i = 0; i < arr.length; i += size) {
+        console.log(arr[i]);
+        result.push(arr.slice(i, i + size));
     }
-    return true;
+    return result;
 }
 
-console.log(mutation(["hello", "hey"]));
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
