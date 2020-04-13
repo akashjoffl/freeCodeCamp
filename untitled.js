69dce3790f8e7c897fc62212aaff62700195f3f5
Akash Jeganath <akashjoffl@icloud.com>
2020-04-14 00:13:56 +0530

    Object Oriented Programming: Use an IIFE to Create a Module

--- untitled.js
+++ untitled.js
@@ -1,12 +1,29 @@
-(function () {
-    console.log("A cozy nest is ready");
+let isCuteMixin = function (obj) {
+    obj.isCute = function () {
+        return true;
+    };
+};
+let singMixin = function (obj) {
+    obj.sing = function () {
+        console.log("Singing to an awesome tune");
+    };
+};
+
+let funModule = (function () {
+    return {
+        isCuteMixin: function (obj) {
+            obj.isCute = function () {
+                return true;
+            };
+        },
+        singMixin: function (obj) {
+            obj.sing = function () {
+                console.log("Singing to an awesome tune");
+            }
+        }
+    }
+
 })();
 
 
-/* function makeNest() {
-  console.log("A cozy nest is ready");
-}
 
-makeNest();
-
-*/
