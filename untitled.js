0ab6decd195c6d3c6d08e8929acc5b294b5fd52a
Akash Jeganath <akashjoffl@icloud.com>
2020-04-15 03:53:53 +0530

    Functional Programming: Pass Arguments to Avoid External Dependence in a Function

--- untitled.js
+++ untitled.js
@@ -1,15 +1,11 @@
 // The global variable
 var fixedValue = 4;
 
-function incrementer() {
-    // Only change code below this line
-    return fixedValue = fixedValue + 1;
-    // Only change code above this line
+// Only change code below this line
+function incrementer(num) {
+    return num + 1  // Only change code above this line
 }
 
+
 console.log(fixedValue);
-
-console.log(incrementer());
-
-// so here, the fixedValue doesn't change
-// the incrementer changed
+console.log(incrementer(fixedValue));
