ec7f0538d9c8781aad2258d92a2488a7260c66c1
Akash Jeganath <akashjoffl@icloud.com>
2020-04-16 04:38:18 +0530

    Functional Programming: Refactor Global Variables Out of Functions

--- untitled.js
+++ untitled.js
@@ -1,11 +1,32 @@
 // The global variable
-var fixedValue = 4;
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
-// Only change code below this line
-function incrementer(num) {
-    return num + 1  // Only change code above this line
+// Change code below this line
+function add(updatedBookList, bookName) {
+    let newXBooks = [...updatedBookList];
+    newXBooks.push(bookName);
+    return newXBooks;
+
+    // Change code above this line
 }
 
+//console.log(add(bookList, 'XXXX'));
+//console.log(bookList);
+// Change code below this line
+function remove(removeBookList, bookName) {
+    var book_index = removeBookList.indexOf(bookName);
+    if (book_index >= 0) {
+        let removeXBook = [...removeBookList]; // cloning the array
+        removeXBook.splice(book_index, 1);
+        return removeXBook;
+        // Change code above this line
+    }
+}
+//console.log(remove(bookList, 'On The Electrodynamics of Moving Bodies'));
 
-console.log(fixedValue);
-console.log(incrementer(fixedValue));
+var newBookList = add(bookList, 'A Brief History of Time');
+//console.log(newBookList);
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+//console.log(bookList);
