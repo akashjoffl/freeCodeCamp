589d85549693865d5294c7b5c9e4abd0c02b1be8
Akash Jeganath <akashjoffl@icloud.com>
2020-04-18 19:55:40 +0530

    Functional Programming: Combine an Array into a String Using the join Method
    oneLine()

--- untitled.js
+++ untitled.js
@@ -1,8 +1,10 @@
 function sentensify(str) {
     // Only change code below this line
-    let sentensifySplit = str.split(/\W/);
-    //console.log(sentensifySplit);
-    let sentensifyJoined = sentensifySplit.join(' ')
-    return sentensifyJoined;
+
+    return str.split(/\W/).join(' ');
+    // let sentensifySplit = str.split(/\W/);
+    // //console.log(sentensifySplit);
+    // let sentensifyJoined = sentensifySplit.join(' ')
+    // return sentensifyJoined;
 }
 console.log(sentensify("The.force.is.strong.with.this.one"));
