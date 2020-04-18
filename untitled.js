8e566c8d4312c542d8b329f0b7546f8efe75f440
Akash Jeganath <akashjoffl@icloud.com>
2020-04-18 09:39:06 +0530

    Functional Programming: Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
    Refractred

--- untitled.js
+++ untitled.js
@@ -1,16 +1,11 @@
 const squareList = (arr) => {
     // Only change code below this line
-    let returnArray = [];
-    let positiveInteger = arr.filter(function (num) {
-        if (Number.isInteger(num) && num > 0) {
-            return num;
-        }
-    }).map(function (num) {
-        return num * num;
-    })
+    let positiveIntegerSquared = arr.filter(num => {
+        if (Number.isInteger(num) && num > 0) return num;
+    }).map(num => { return num * num });
     // filterMethod to find the positive integers, and using the output to .map() to find the map();
 
-    return positiveInteger;
+    return positiveIntegerSquared;
     // Only change code above this line
 };
 
