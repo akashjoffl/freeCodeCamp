8536b0ed93b838bd449d8c665dad97aa7759bd3c
Akash Jeganath <akashjoffl@icloud.com>
2020-04-18 11:51:11 +0530

    Functional Programming: Return a Sorted Array Without Changing the Original Array
    ES6;

--- untitled.js
+++ untitled.js
@@ -1,7 +1,7 @@
 var globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
     // Only change code below this line
-    return arr.slice().sort();
+    return [...arr].sort();
     // Only change code above this line
 }
 console.log(nonMutatingSort(globalArray));
