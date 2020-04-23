12abb1cd5f8db6611ea58d2e7995790ca1d75099
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 04:23:14 +0530

    Intermediate Algorithm Scripting: Pig Latin
    refractored();

--- untitled.js
+++ untitled.js
@@ -1,15 +1,22 @@
 function translatePigLatin(str) {
   let vowels = "aeioe".split("");
 
+  function isVowel(letter) {
+    return vowels.indexOf(letter);
+  }
+
   for (let i = 0; i < str.length; i++) {
-    let individualLetter = str[i]
-    if (vowels.indexOf(individualLetter) != -1 && i === 0) {
+    let individualLetter = str[i];
+
+    if (isVowel(individualLetter) != -1 && i === 0) {
       return str + "way";
     }
-    if (vowels.indexOf(individualLetter) != -1) {
+    if (isVowel(individualLetter) != -1) {
       return str.slice(i) + str.slice(0, i) + "ay";
     }
   }
+
+
   return str + "ay";
 }
 
