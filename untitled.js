c2bfe5c690599671e9f12c128e5a862819507025
Akash Jeganath <akashjoffl@icloud.com>
2020-04-23 20:51:21 +0530

    Intermediate Algorithm Scripting: Wherefore art thou

--- untitled.js
+++ untitled.js
@@ -1,15 +1,28 @@
-function destroyer(arr) {
-  let survivingElements = [];
-  let battleField = Object.values(arguments)[0];
-  let elementToDestroy = Object.values(arguments).splice(1);
-
-  battleField.forEach(positionToRecon => {
-    if (elementToDestroy.indexOf(positionToRecon) === -1) {
-      survivingElements.push(positionToRecon);
+function whatIsInAName(collections, source) {
+  var matchingResult = [];
+  // Only change code below this line
+  collections.forEach(collection => {
+   console.log(collection)
+    let index = 0;
+    for (let key in source) {
+    //  console.log(collection[key]) //122 un 122
+      if (collection[key] === source[key]) {
+        index += 1;
+      }
+      if (index === Object.keys(source).length) {
+        matchingResult.push(collection);
+      }
     }
-  });
-  return survivingElements;
+  })
+  // Only change code above this line
+  return matchingResult;
 }
 
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
-// [1, 1]
+let result = whatIsInAName([
+  { "apple": 1, "bat": 2 }, 
+  { "bat": 2 }, 
+  { "apple": 1, "bat": 2, "cookie": 2 }
+    ], 
+  { "apple": 1, "bat": 2 })
+
+console.log(result);
