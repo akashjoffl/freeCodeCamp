b3fb075154a5fdb3f4046c85f5696ee04d9b6f04
Akash Jeganath <akashjoffl@icloud.com>
2020-04-26 18:22:58 +0530

    Intermediate Algorithm Scripting: Convert HTML Entities

--- untitled.js
+++ untitled.js
@@ -1,16 +1,33 @@
-function uniteUnique(collection) {
-  let finalCollection = [];
-  let values = Object.values(arguments);
-
-  values.forEach(groupOfGroups => {
-    groupOfGroups.forEach(individualValues => {
-      if (finalCollection.indexOf(individualValues) === -1) {
-        finalCollection.push(individualValues);
-      }
-    });
-  });
-  return finalCollection;
+function convertCharecter(letter) {
+  if (letter === "&") {
+    return "&amp;";
+  } else if (letter === "<") {
+    return "&lt;";
+  } else if (letter === ">") {
+    return "&gt;";
+  } else if (letter === '"') {
+    return "&quot;";
+  } else if (letter === "'") {
+    return "&apos;";
+  } else if (letter == "<>") {
+    return "&lt;&gt;";
+  }
 }
 
-let result = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+function convertHTML(str) {
+  let charectersToCheckFor = ["&", "<", ">", "'", '"', "<>"];
+
+  for (let i = 0; i < str.length; i++) {
+    // console.log(str[i])
+    if (charectersToCheckFor.indexOf(str[i]) != -1) {
+      let symbolToConvert = str[i];
+      str = str.slice(0, i) +
+        convertCharecter(symbolToConvert) +
+        str.slice(i + 1)
+    };
+  }
+  return str;
+}
+
+let result = convertHTML("Dolce & Gabbana");
 console.log(result);
