2007cae9ccfb8870fdcb33d783cd9a25bdc73f4e
Akash Jeganath <akashjoffl@icloud.com>
2020-04-28 02:19:14 +0530

    Intermediate Algorithm Scripting: Drop it
    codeRefactored();

--- untitled.js
+++ untitled.js
@@ -1,14 +1,8 @@
 function dropElements(arr, func) {
-  let result = [];
-
   for (let i = 0; i < arr.length; i += 1) {
-    console.log(arr[i], func(arr[i]), arr.slice(i))
-    if (func(arr[i])) {
-      return arr.slice(i)
-    }
+    if (func(arr[i])) { return arr.slice(i) }
   }
-
-  return result;
+  return [];
 }
 
 let result = dropElements([1, 2, 3, 4], function (n) { return n >= 3; });
