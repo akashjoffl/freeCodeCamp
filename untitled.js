2d469b3a5985f3de5d4d8512c207f768247d5ae1
Akash Jeganath <akashjoffl@icloud.com>
2020-04-28 02:59:50 +0530

    Intermediate Algorithm Scripting: Steamroller
    FCC();

--- untitled.js
+++ untitled.js
@@ -1,9 +1,11 @@
-function dropElements(arr, func) {
-  for (let i = 0; i < arr.length; i += 1) {
-    if (func(arr[i])) { return arr.slice(i) }
-  }
-  return [];
+function steamrollArray(val, flatArr = []) {
+  val.forEach(item => {
+    if (Array.isArray(item)) steamrollArray(item, flatArr);
+    else flatArr.push(item);
+  });
+
+  return flatArr;
 }
 
-let result = dropElements([1, 2, 3, 4], function (n) { return n >= 3; });
+let result = steamrollArray([1, [2], [3, [[4]]]]);
 console.log(result);
