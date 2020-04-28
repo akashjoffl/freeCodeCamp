f290fdf91746ae06cd601eb914b76cb6f6476b20
Akash Jeganath <akashjoffl@icloud.com>
2020-04-28 23:35:01 +0530

    Intermediate Algorithm Scripting: Binary Agents

--- untitled.js
+++ untitled.js
@@ -1,11 +1,14 @@
-function steamrollArray(val, flatArr = []) {
-  val.forEach(item => {
-    if (Array.isArray(item)) steamrollArray(item, flatArr);
-    else flatArr.push(item);
-  });
-
-  return flatArr;
+function binaryAgent(str) {
+  return str.split(" ").map(binaryDataPoint => {
+    let charecterCode = parseInt(binaryDataPoint, 2);
+    let dedicatedLetter = String.fromCharCode(charecterCode)
+    return dedicatedLetter;
+  }).join("");
 }
 
-let result = steamrollArray([1, [2], [3, [[4]]]]);
+
+let binaryData = "01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001";
+
+
+let result = binaryAgent(binaryData);
 console.log(result);
