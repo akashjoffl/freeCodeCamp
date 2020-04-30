97009420f21c7a70671adeb5917e3f9fb3bd5eb4
Akash Jeganath <akashjoffl@icloud.com>
2020-04-30 23:26:32 +0530

    Intermediate Algorithm Scripting: Everything Be True
    higherOrderFunction();

--- untitled.js
+++ untitled.js
@@ -1,17 +1,21 @@
 function truthCheck(collection, pre) {
-  let result = true;
-  for (let i = 0; i < collection.length; i += 1) {
-    // console.log(collection[i][pre]);
-    if (collection[i][pre] === undefined ||
-      collection[i][pre] === 0 ||
-      collection[i][pre] === null ||
-      collection[i][pre] === "" ||
-      Number.isNaN(collection[i][pre]) // you pass the element as argument
-    ) {
-      result = false;
-    }
-  }
-  return result;
+
+  return collection.every(function (dataPoint) {
+    return dataPoint[pre];
+  })
+  //   let result = true;
+  //   for (let i = 0; i < collection.length; i += 1) {
+  //    // console.log(collection[i][pre]);
+  //     if (collection[i][pre] === undefined ||
+  //         collection[i][pre] === 0 ||
+  //         collection[i][pre] === null ||
+  //         collection[i][pre] === "" ||
+  //         Number.isNaN(collection[i][pre])
+  //     ) {
+  //       result = false;
+  //     }
+  //   }
+  //   return result;
 }
 
 let finalAnswer = truthCheck([{ "single": "double" }, { "single": NaN }], "single")
