690f2132d961a77b984e98e214df466992f8c015
Akash Jeganath <akashjoffl@icloud.com>
2020-04-30 23:58:14 +0530

    Intermediate Algorithm Scripting: Arguments Optional

--- untitled.js
+++ untitled.js
@@ -1,23 +1,41 @@
-function truthCheck(collection, pre) {
+function addTogether() {
 
-  return collection.every(function (dataPoint) {
-    return dataPoint[pre];
-  })
-  //   let result = true;
-  //   for (let i = 0; i < collection.length; i += 1) {
-  //    // console.log(collection[i][pre]);
-  //     if (collection[i][pre] === undefined ||
-  //         collection[i][pre] === 0 ||
-  //         collection[i][pre] === null ||
-  //         collection[i][pre] === "" ||
-  //         Number.isNaN(collection[i][pre])
-  //     ) {
-  //       result = false;
-  //     }
-  //   }
-  //   return result;
+  let firstArgument = arguments[0];
+
+
+  if (arguments.length > 1) {
+    let secondArgument = arguments[1];
+    if (
+      argumentCheck(firstArgument) !== undefined &&
+      argumentCheck(secondArgument) !== undefined) {
+      return firstArgument + secondArgument
+    } else {
+      return undefined;
+    }
+  } else if (arguments.length === 1) {
+    if (argumentCheck(firstArgument) === undefined) {
+      return undefined;
+    } else {
+      return function (secondArgument) {
+        if (
+          argumentCheck(firstArgument) !== undefined &&
+          argumentCheck(secondArgument) !== undefined) {
+          return firstArgument + secondArgument
+        } else {
+          return undefined;
+        }
+      }
+    }
+  }
 }
 
-let finalAnswer = truthCheck([{ "single": "double" }, { "single": NaN }], "single")
+function argumentCheck(argument) {
+  if (typeof argument === 'number') {
+    return argument;
+  } else {
+    return undefined;
+  }
+}
 
-console.log(finalAnswer);
+let result = addTogether(2);
+console.log(result);
