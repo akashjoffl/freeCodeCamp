28cd6c4d70a9144c4e68dce3a2dac48c1122883b
Akash Jeganath <akashjoffl@icloud.com>
2020-04-30 23:20:43 +0530

    Intermediate Algorithm Scripting: Everything Be True

--- untitled.js
+++ untitled.js
@@ -1,14 +1,19 @@
-function binaryAgent(str) {
-  return str.split(" ").map(binaryDataPoint => {
-    let charecterCode = parseInt(binaryDataPoint, 2);
-    let dedicatedLetter = String.fromCharCode(charecterCode)
-    return dedicatedLetter;
-  }).join("");
+function truthCheck(collection, pre) {
+  let result = true;
+  for (let i = 0; i < collection.length; i += 1) {
+    // console.log(collection[i][pre]);
+    if (collection[i][pre] === undefined ||
+      collection[i][pre] === 0 ||
+      collection[i][pre] === null ||
+      collection[i][pre] === "" ||
+      Number.isNaN(collection[i][pre]) // you pass the element as argument
+    ) {
+      result = false;
+    }
+  }
+  return result;
 }
 
+let finalAnswer = truthCheck([{ "single": "double" }, { "single": NaN }], "single")
 
-let binaryData = "01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001";
-
-
-let result = binaryAgent(binaryData);
-console.log(result);
+console.log(finalAnswer);
