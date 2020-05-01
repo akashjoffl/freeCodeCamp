4e8fcb7f24c154ea7b11e5f7237a0ff50e86d712
Akash Jeganath <akashjoffl@icloud.com>
2020-05-02 02:27:54 +0530

    Intermediate Algorithm Scripting: Map the Debris

--- untitled.js
+++ untitled.js
@@ -1,33 +1,40 @@
-var Person = function (firstAndLast) {
-  // Complete the method below and implement the others similarly
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
 
-  this.getFullName = function () {
-    return firstAndLast;
-  };
+  let orbitalPeriodResults = [];
 
-  this.getFirstName = function () {
-    return firstAndLast.split(" ")[0];
-  };
+  arr.forEach(function (dataPoint) {
+    let translatedDataPoint = {};
+    let twoTimePie = Math.PI * 2;
+    let earthRadiusPlusAverageAltitude = earthRadius + dataPoint.avgAlt;
+    let topOfDivider = earthRadiusPlusAverageAltitude *
+      earthRadiusPlusAverageAltitude *
+      earthRadiusPlusAverageAltitude;
+    console.log(earthRadiusPlusAverageAltitude);
 
-  this.getLastName = function () {
-    return firstAndLast.split(" ")[1];
-  };
+    let numberToSquare = topOfDivider / GM;
+    let squareResult = Math.sqrt(numberToSquare);
 
-  this.setFullName = function (newFullName) {
-    return firstAndLast = newFullName;
-  };
+    let orbitalPeriodResult = twoTimePie * squareResult;
+    console.log(orbitalPeriodResult);
 
-  this.setFirstName = function (newFirstName) {
-    return firstAndLast = newFirstName + " " + this.getLastName();
-  }
+    translatedDataPoint.name = dataPoint.name;
+    translatedDataPoint.orbitalPeriod = Math.round(orbitalPeriodResult)
 
-  this.setLastName = function (newLastName) {
-    return firstAndLast = this.getFirstName() + " " + newLastName;
-  }
+    orbitalPeriodResults.push(translatedDataPoint);
 
-  return firstAndLast;
-};
+  });
 
-var bob = new Person('Bob Ross');
+  return orbitalPeriodResults;
+}
 
-console.log(bob.getFullName());
+let inputResult = orbitalPeriod(
+  [
+    { name: "iss", avgAlt: 413.6 },
+    { name: "hubble", avgAlt: 556.7 },
+    { name: "moon", avgAlt: 378632.553 }
+  ]
+);
+
+console.log(inputResult);
