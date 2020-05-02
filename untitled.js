4431cbb24bc947cdd447edad074346ce0a91d4c6
Akash Jeganath <akashjoffl@icloud.com>
2020-05-02 16:14:47 +0530

    JavaScript Algorithms and Data Structures Projects: Palindrome Checker

--- untitled.js
+++ untitled.js
@@ -1,40 +1,14 @@
-function orbitalPeriod(arr) {
-  var GM = 398600.4418;
-  var earthRadius = 6367.4447;
-
-  let orbitalPeriodResults = [];
-
-  arr.forEach(function (dataPoint) {
-    let translatedDataPoint = {};
-    let twoTimePie = Math.PI * 2;
-    let earthRadiusPlusAverageAltitude = earthRadius + dataPoint.avgAlt;
-    let topOfDivider = earthRadiusPlusAverageAltitude *
-      earthRadiusPlusAverageAltitude *
-      earthRadiusPlusAverageAltitude;
-    console.log(earthRadiusPlusAverageAltitude);
-
-    let numberToSquare = topOfDivider / GM;
-    let squareResult = Math.sqrt(numberToSquare);
-
-    let orbitalPeriodResult = twoTimePie * squareResult;
-    console.log(orbitalPeriodResult);
-
-    translatedDataPoint.name = dataPoint.name;
-    translatedDataPoint.orbitalPeriod = Math.round(orbitalPeriodResult)
-
-    orbitalPeriodResults.push(translatedDataPoint);
-
-  });
-
-  return orbitalPeriodResults;
+function palindrome(str) {
+  let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
+  let reversedString = polishedString.split("").reverse().join("");
+  console.log(polishedString, reversedString)
+  if (polishedString != reversedString) {
+    return false;
+  }
+  return true;
 }
 
-let inputResult = orbitalPeriod(
-  [
-    { name: "iss", avgAlt: 413.6 },
-    { name: "hubble", avgAlt: 556.7 },
-    { name: "moon", avgAlt: 378632.553 }
-  ]
-);
 
-console.log(inputResult);
+
+let result = palindrome("A man, a plan, a canal. Panama");
+console.log(result);
