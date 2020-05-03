32660c57ac88d414cfe8f96d3ebfae43a90e28ee
Akash Jeganath <akashjoffl@icloud.com>
2020-05-03 16:15:38 +0530

    JavaScript Algorithms and Data Structures Projects: Telephone Number Validator
    codeRefactored();

--- untitled.js
+++ untitled.js
@@ -1,21 +1,12 @@
 function telephoneCheck(str) {
-  if (str.indexOf("(") === -1 && str.indexOf(")") > -1) {
-    return false;
-  }
-
-  if (str[0] === "-") {
-    return false;
-  }
-
-  if (str.indexOf(")") - str.indexOf("(") >= 5) {
-    return false;
-  }
+  if (str.indexOf("(") === -1 && str.indexOf(")") > -1) return false;
+  if (str[0] === "-") return false;
+  if (str.indexOf(")") - str.indexOf("(") >= 5) return false;
 
   let polishedPhone = str.replace(/-| /g, "");
   if (polishedPhone.indexOf("(") < polishedPhone.indexOf(")")) {
     polishedPhone = polishedPhone.replace(/\(|\)/g, "");
   }
-  console.log(polishedPhone);
 
   if (polishedPhone.length === 10) {
     return true;
