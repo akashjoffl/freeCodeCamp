9dc890b2841bc65eb7fe205b754bb133ffce8b92
Akash Jeganath <akashjoffl@icloud.com>
2020-05-03 17:43:27 +0530

    JavaScript Algorithms and Data Structures Projects: Cash Register
    FCC*

--- untitled.js
+++ untitled.js
@@ -1,20 +1,85 @@
-function telephoneCheck(str) {
-  if (str.indexOf("(") === -1 && str.indexOf(")") > -1) return false;
-  if (str[0] === "-") return false;
-  if (str.indexOf(")") - str.indexOf("(") >= 5) return false;
+// Create an array of objects which hold the denominations and their values
+var denom = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
 
-  let polishedPhone = str.replace(/-| /g, "");
-  if (polishedPhone.indexOf("(") < polishedPhone.indexOf(")")) {
-    polishedPhone = polishedPhone.replace(/\(|\)/g, "");
+function checkCashRegister(price, cash, cid) {
+  var output = { status: null, change: [] };
+  var change = cash - price;
+
+  // Transform CID array into drawer object
+  var register = cid.reduce(
+    function (acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  // Handle exact change
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
   }
 
-  if (polishedPhone.length === 10) {
-    return true;
-  } else if (polishedPhone.length === 11 && polishedPhone[0] === "1") {
-    return true;
+  // Handle obvious insufficient funds
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
   }
-  return false;
+
+  // Loop through the denomination array
+  var change_arr = denom.reduce(function (acc, curr) {
+    var value = 0;
+    // While there is still money of this type in the drawer
+    // And while the denomination is larger than the change remaining
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+
+      // Round change to the nearest hundreth deals with precision errors
+      change = Math.round(change * 100) / 100;
+    }
+    // Add this denomination to the output only if any was used.
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+  // If there are no elements in change_arr or we have leftover change, return
+  // the string "Insufficient Funds"
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Here is your change, ma'am.
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
 }
 
-let result = telephoneCheck("-1 (757) 622-7382")
-console.log(result);
+// test here
+checkCashRegister(19.5, 20.0, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90.0],
+  ["FIVE", 55.0],
+  ["TEN", 20.0],
+  ["TWENTY", 60.0],
+  ["ONE HUNDRED", 100.0]
+]);
